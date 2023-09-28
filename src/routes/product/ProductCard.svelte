7899eb9ec90f56d33bcd2056447dc4ea1c29593f
iladiro <dirosa.ilaria@gmail.com>
2023-09-28 17:51:56 +0200

    styling product card

--- src/routes/product/ProductCard.svelte
+++ src/routes/product/ProductCard.svelte
@@ -20,7 +20,7 @@
 		<a href={details.href} class="no-underline">{details.title}</a>
 	</div>
 	<div class="flex items-stretch gap-2">
-		<button type="button" on:click={openModal} class="p-2 bg-[#f8e5e6] rounded-sm text-[#454545] uppercase no-underline">Dettagli</button>
-		<a href={details.href} target="_blank" class="p-2 bg-[#f8e5e6] rounded-sm text-[#454545] uppercase no-underline">Etsy shop</a>
+		<button type="button" on:click={openModal} class="p-2 bg-[#f8e5e6] transition-all ease-in-out hover:shadow-md duration-300 shadow-sm rounded-sm text-[#454545] uppercase no-underline text-sm">Dettagli</button>
+		<a href={details.href} target="_blank" class="p-2 bg-[#f8e5e6] transition-all ease-in-out hover:shadow-md duration-300 shadow-sm rounded-sm text-[#454545] uppercase no-underline text-sm">Etsy shop</a>
 	</div>
 </div>
