836289599142b31e922e739818041d823c8e9bac
iladiro <dirosa.ilaria@gmail.com>
2023-09-28 17:17:17 +0200

    styling modal

--- src/routes/product/ProductList.svelte
+++ src/routes/product/ProductList.svelte
@@ -6,7 +6,7 @@
     import Modal, { bind } from 'svelte-simple-modal';
     import Popup from '../widgets/Popup.svelte';
 
-    const modal = writable();
+    const modal = writable(null);
 
     const openModal = (event: any) => {
         modal.set(bind(Popup, { content: event.detail }))
@@ -23,5 +23,11 @@
     <div class="text-center">
         <a href="https://www.etsy.com/it/shop/IlCrochetDili" target="_blank" class="text-sm font-normal p-2 bg-[#f8e5e6] rounded-sm text-[#454545] no-underline">Vai al negozio</a>
     </div>
-    <Modal show={$modal}></Modal>
+    <Modal 
+        show={$modal}
+        classBg="fixed top-0 left-0 w-screen h-screen flex flex-col justify-center bg-orange-100/[.9]"
+        classWindowWrap="relative m-2 max-h-full"
+        classWindow="relative w-40 max-w-full max-h-full my-2 mx-auto text-orange-200 rounded shadow-md bg-indigo-900"
+        classContent="relative p-2 overflow-auto">
+    </Modal>
 </section>
