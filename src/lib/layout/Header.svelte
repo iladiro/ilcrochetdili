7a277569c9fc1e2ac46cdf3f9b3662f452fe3551
iladiro <dirosa.ilaria@gmail.com>
2023-09-29 14:49:24 +0200

    responsive

--- src/lib/layout/Header.svelte
+++ src/lib/layout/Header.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <header class="w-full p-4">
-    <div class="flex justify-center relative">
-        <a href="https://kit.svelte.dev">
+    <div class="flex items-center justify-center relative flex-col md:flex-row">
+        <a href="https://kit.svelte.dev" class="mb-5 md:mb-0">
             <img src={logo} alt="IlCrochetDiIli" width="250" />
         </a>
-        <div class="absolute top-[50%] right-0 translate-y-[-50%]">
+        <div class="md:absolute md:top-[50%] md:right-0 md:translate-y-[-50%]">
             <SocialChannels></SocialChannels>
         </div>
     </div>
